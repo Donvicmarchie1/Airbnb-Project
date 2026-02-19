0575434b1062ac050890efd45b0cce41f99fe63d
Marchie Victor <marchievictor87@gmail.com>
2026-02-19 08:58:23 -0800

    fix: install embla autoplay and fix types

--- src/component/EmblaCarousel.tsx
+++ src/component/EmblaCarousel.tsx
@@ -1,11 +1,6 @@
 import { useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
-import grotSpidey from "../assets/grotspidy.jpg"
-import blueySpidey from "../assets/blueyspidey.jpg"
-import twoSpidey from "../assets/twospidy.jpg"
-
-
 
 export function EmblaCarousel() {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()])
@@ -22,15 +17,16 @@ export function EmblaCarousel() {
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          <div className="embla__slide">{grotSpidey}</div>
-          <div className="embla__slide">{blueySpidey}</div>
-          <div className="embla__slide">{twoSpidey}</div>
+          <div className="embla__slide">Slide 1</div>
+          <div className="embla__slide">Slide 2</div>
+          <div className="embla__slide">Slide 3</div>
         </div>
       </div>
 
       <button className="embla__prev" onClick={goToPrev}>
         Scroll to prev
       </button>
+
       <button className="embla__next" onClick={goToNext}>
         Scroll to next
       </button>
